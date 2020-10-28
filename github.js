70b78b95bd556afaceb52e1649058fb1dd52e93b
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-10-28 11:37:37 -0400

    [js] api appending

--- github.js
+++ github.js
@@ -1,10 +1,21 @@
+const container = document.querySelector('.container');
+
 fetch('https://api.github.com/users/zfinnan')
 .then(response => {
     return response.json();
-});
+})
 .then(gitHubData => {
-    console.log(gitHubData);
-});
+    const zachObj = {
+        bio: gitHubData.bio,
+        username: gitHubData.login,
+        name: gitHubData.name
+    }
+    console.log(zachObj);
+
+    const newElement = document.createElement('p');
+    newElement.textContent = zachObj.name;
+    container.appendChild(newElement);
+})
 .catch(error => {
     console.log(error);
 })
